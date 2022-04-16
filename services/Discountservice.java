7fc3f57c50983aa4a57ca1f20944f89e0857cdea
gowtham kumar <kumara1215@gmail.com>
2022-04-16 17:47:40 +0530

    update the price

--- services/Discountservice.java
+++ services/Discountservice.java
@@ -1,9 +1,6 @@
 public class service {
 	public static void main (String [] args) {
-<<<<<<< HEAD
-=======
 		int price = 200;
->>>>>>> 8b741cb (update the price data)
 		String brand = oneplus;
 		
 	}
